728a48f6a8bf80e38318239ec7fba0d540b4b92f
ashmit <ashmitbhardwaj05@gmail.com>
2024-03-27 08:08:40 +0530

    best custom config so far

--- .config/nvim/lua/custom/chadrc.lua
+++
@@ -1,7 +0,0 @@
----@type ChadrcConfig
-local M = {}
-
-M.ui = { theme = 'gruvbox' }
-M.plugins = "custom.plugins"
-
-return M
--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,17 +1,2 @@
-local opt = vim.opt
-
-opt.relativenumber = true
-
--- Remap j to jzz and G to Gzz
-vim.cmd [[
-    " nnoremap j jzz
-    " nnoremap k kzz
-    nnoremap G Gzz
-    nnoremap o zzo
-]]
-
-vim.opt.scrolloff = 8
-
--- -- Transparent Background
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+require("jacksparrow.remap")
+print("savvy")
