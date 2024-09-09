7d2c834c08666442845da6441031e4da5b6ea42c
ashmit <ashmitbhardwaj05@gmail.com>
2024-09-10 00:51:42 +0530

    cleaning up

--- .config/nvim/lua/jacksparrow/packer.lua
+++ .config/nvim/lua/jacksparrow/packer.lua
@@ -111,7 +111,7 @@ return require('packer').startup(function(use)
 
 	use {
 		'nvim-lualine/lualine.nvim',
-		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true },
 	}
 
   use 'nvim-tree/nvim-web-devicons'
